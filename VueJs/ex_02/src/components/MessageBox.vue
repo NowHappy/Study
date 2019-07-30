4fb447844573ff5f360945ce7bfb1c1679a77fb2
Joy <rlfmalehd@gmail.com>
2019-07-30 10:45:36 +0900

    props example
    practicing

---
+++ VueJs/ex_02/src/components/MessageBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 v-if="passedData=='banana'">{{MyMsg}}</h1>
+        <h1 v-else>{{passedData}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props : {
+        passedData: {
+                    type: String
+                }         
+    },
+    data() {
+        return {
+            MyMsg : 'banana is delicous'
+        }
+    }
+}
+</script>
